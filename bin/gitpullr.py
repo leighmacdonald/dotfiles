44595b5688a476378f538d233211c1a83e5e54c4
Leigh MacDonald <leigh@cudd.li>
2012-11-07 15:49:34 -0700

    Fix for <=2.6 support

--- bin/gitpullr.py
+++ bin/gitpullr.py
@@ -4,6 +4,7 @@ from os import listdir, getcwd, chdir
 from os.path import exists, join
 from logging import getLogger
 from subprocess import Popen
+from sys import argv
 
 log = getLogger("gitpullr")
 
@@ -26,15 +27,20 @@ if __name__ == "__main__":
     from logging import basicConfig, INFO
 
     basicConfig(level=INFO)
-    from argparse import ArgumentParser
+    try:
+        from argparse import ArgumentParser
 
-    arg_parser = ArgumentParser(prog="gitpullr.py",
-        description="Find git repos under a path and update them")
-    arg_parser.add_argument('paths', metavar="PATH", nargs="+", help="Path to scan for repos")
-    args = arg_parser.parse_args()
+        arg_parser = ArgumentParser(prog="gitpullr.py",
+            description="Find git repos under a path and update them")
+        arg_parser.add_argument('paths', metavar="PATH", nargs="+", help="Path to scan for repos")
+        args = arg_parser.parse_args()
+        paths = args.paths
+    except ImportError:
+        # hack for <2.6
+        paths = [argv[1:]]
     ret_val = 1
     try:
-        for path in args.paths:
+        for path in paths:
             update_git_repos(path)
     except Exception as err:
         log.error(err)
